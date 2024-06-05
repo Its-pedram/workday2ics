283a9ece1d087ca5ec07ac728314ec106a6230ae
its-pedram <55721066+Its-pedram@users.noreply.github.com>
2024-06-05 09:37:40 -0700

    some more improvements

--- src/lib/calendar_utils.ts
+++ src/lib/calendar_utils.ts
@@ -1,55 +1,62 @@
 import ExcelJS from 'exceljs';
 
 interface Course {
-    courseName: string;
-    credits: number;
-    format: string;
-    instructor: string;
-    startDate: string;
-    endDate: string;
-    meetingDays: Array<string>;
-    startTime: string;
-    endTime: string;
-    location: string;
+	courseName: string;
+	credits: number;
+	format: string;
+	instructor: string;
+	startDate: string;
+	endDate: string;
+	meetingDays: Array<string>;
+	startTime: string;
+	endTime: string;
+	location: string;
 }
 
 export async function parseWorkdayCal(file: File) {
-    const workbook = new ExcelJS.Workbook();
-    await workbook.xlsx.load(await file.arrayBuffer());
-    const calWorksheet = workbook.getWorksheet('View My Saved Schedules');
-    
-    let courses = await parseCourses(calWorksheet!);
+	const workbook = new ExcelJS.Workbook();
+	await workbook.xlsx.load(await file.arrayBuffer());
+	const calWorksheet = workbook.getWorksheet('View My Saved Schedules');
+
+	let courses = parseCourses(calWorksheet!);
+    // console.log(courses[0].startDate);
 }
 
-export async function parseCourses(calWorksheet: ExcelJS.Worksheet): Promise<Array<Course>> {
-    let courses: Array<Course> = [];
-    for (let i = 3; i < calWorksheet.rowCount; i++) {
-        let row = calWorksheet.getRow(i);
-        let meetingPattern = await parseMeetingPatterns(row.getCell('J').value as string);
-        let course: Course = {
-            courseName: row.getCell('D').value as string,
-            credits: row.getCell('C').value as number,
-            format: row.getCell('F').value as string,
-            instructor: row.getCell('G').value as string,
-            startDate: row.getCell('H').value as string,
-            endDate: row.getCell('I').value as string,
-            meetingDays: meetingPattern[0].split(" "),
-            startTime: meetingPattern[1],
-            endTime: meetingPattern[2],
-            location: meetingPattern[3],
-        }
+export function parseCourses(calWorksheet: ExcelJS.Worksheet): Array<Course> {
+	let courses: Array<Course> = [];
+	for (let i = 3; i < calWorksheet.rowCount; i++) {
+		let row = calWorksheet.getRow(i);
+		let meetingPattern = parseMeetingPatterns(row.getCell('J').value as string);
+		let course: Course = {
+			courseName: row.getCell('D').value as string,
+			credits: row.getCell('C').value as number,
+			format: row.getCell('F').value as string,
+			instructor: row.getCell('G').value as string,
+            startDate: new Date(meetingPattern[0]).toISOString(),
+            endDate:  new Date(meetingPattern[1]).toISOString(),
+			meetingDays: meetingPattern[2].split(' '),
+			startTime: meetingPattern[3],
+			endTime: meetingPattern[4],
+			location: meetingPattern[5]
+		};
+		courses.push(course);
         console.log(course);
-        courses.push(course);
-    }
-    return courses;
+	}
+	return courses;
 }
 
-export async function parseMeetingPatterns(meetingPattern: string): Promise<Array<string>> {
-    let pattern = meetingPattern.split(" | ");
+export function parseMeetingPatterns(meetingPattern: string): Array<string> {
+	let pattern = meetingPattern.split(' | ');
     return [
-        pattern[1],
-        pattern[2].split(" - ")[0],
-        pattern[2].split(" - ")[1],
-        pattern[3].replace(/-/g, ' ')
+        cleanPattern(pattern[0].split(' - ')[0]),
+        cleanPattern(pattern[0].split(' - ')[1]),
+        cleanPattern(pattern[1]),
+        cleanPattern(pattern[2].split(' - ')[0]),
+        cleanPattern(pattern[2].split(' - ')[1]),
+        pattern[3] ? cleanPattern(pattern[3].replace(/-/g, ' ')) : 'Roomless'
     ];
+}
+
+export function cleanPattern(pattern: string): string {
+    return pattern.replace(/\|/g, '').trim();
 }
