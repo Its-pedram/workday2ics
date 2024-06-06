bb634c5ee1533e4a54e361fccd52f5ef14df06e4
Zenia Lau <zenialaulau.lau@gmail.com>
2024-06-05 20:31:50 -0700

    added docs to functions

--- src/lib/ics_utils.ts
+++ src/lib/ics_utils.ts
@@ -3,6 +3,11 @@ import { generateBlob } from './blob';
 import type { Course } from './parsing_utils';
 
 export class iCalendar {
+	/**
+	 * Generates a calendar file in iCalendar format based on the provided courses.
+	 * @param courses - An array of Course objects representing the courses to include in the calendar.
+	 * @returns A Promise that resolves to void.
+	 */
 	static async generateCalendarFile(courses: Array<Course>): Promise<void> {
 		const courseEvents = courses.map(async (course) => {
 			let eventValue = await this.makeCalendarEvent(
@@ -47,6 +52,17 @@ export class iCalendar {
 		});
 	}
 
+	/**
+	 * Creates a calendar event using the provided parameters.
+	 *
+	 * @param title - The title of the event.
+	 * @param description - The description of the event.
+	 * @param start - The start time of the event in milliseconds.
+	 * @param end - The end time of the event in milliseconds.
+	 * @param location - The location of the event.
+	 * @param recurrenceRule - The recurrence rule for the event.
+	 * @returns A promise that resolves to the created event as a string, or rejects with an error.
+	 */
 	static async makeCalendarEvent(
 		title: string,
 		description: string,
@@ -77,12 +93,24 @@ export class iCalendar {
 		});
 	}
 
+	/**
+	 * Formats a date string in a specific ISO format.
+	 *
+	 * @param date - The date to be formatted.
+	 * @returns The formatted date string.
+	 */
 	static formatDateString(date: Date): string {
 		const localDate = new Date(date.toLocaleDateString());
 		localDate.setDate(localDate.getDate() + 1);
 		return localDate.toISOString().replace(/[-:.]/g, '').replace('000Z', 'Z');
 	}
 
+	/**
+	 * Adjusts the start date to the next available meeting day based on the provided meeting days.
+	 * @param startDate - The initial start date.
+	 * @param meetingDays - An array of meeting days represented as strings (e.g., ['MO', 'WE', 'FR']).
+	 * @returns The adjusted start date.
+	 */
 	static adjustStartDate(startDate: Date, meetingDays: string[]): Date {
 		const DAYS = ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA'];
 		const meetingDaysIndices = meetingDays.map((day) => DAYS.indexOf(day));
@@ -94,12 +122,13 @@ export class iCalendar {
 		return adjustedDate;
 	}
 
+	/**
+	 * Converts a given date and time string to Unix milliseconds.
+	 * @param date - The date object representing the date.
+	 * @param timeString - The time string in the format "HH:mm a.m./p.m.".
+	 * @returns The Unix milliseconds representation of the given date and time.
+	 */
 	static convertToUnixMilliseconds(date: Date, timeString: string): number {
-		const time = this.convertTimeStringToDate(date, timeString);
-		return time.getTime();
-	}
-
-	static convertTimeStringToDate(date: Date, timeString: string): Date {
 		let [time, period] = timeString.split(' ');
 		let [hours, minutes] = time.split(':').map(Number);
 
@@ -112,6 +141,6 @@ export class iCalendar {
 		let newDate = new Date(date);
 		newDate.setHours(hours, minutes);
 
-		return newDate;
+		return newDate.getTime();
 	}
 }
